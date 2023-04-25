e8c1f29b39b2258b132f9f4570c80fc55d22b62c
Polina Ryabova <polinaryabova.2@gmail.com>
2023-04-25 17:53:37 +0500

    add hiding of the element if it's active

--- js/accordion.js
+++ js/accordion.js
@@ -4,15 +4,9 @@ const lists = document.querySelectorAll('.feature-sub');
 btns.forEach((btnItem, index) => {
     btnItem.addEventListener('click', () => {
         //смена стрелок
-        btns.forEach((btnItem) => {
-            btnItem.classList.remove('feature__link_active');
-        });
-        btnItem.classList.add('feature__link_active');
+        btnItem.classList.toggle('feature__link_active');
 
         //смена показа блока со списком характеристик
-        lists.forEach((listItem) => {
-            listItem.classList.add('hidden');
-        });
-        lists[index].classList.remove('hidden');
+        lists[index].classList.toggle('hidden');
     });
 });
