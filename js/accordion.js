f76c860ac72c090fceca18b213005d19a1f4b0a8
Polina Ryabova <polinaryabova.2@gmail.com>
2023-04-25 17:46:24 +0500

    Add sources

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll('.feature__link');
+const lists = document.querySelectorAll('.feature-sub');
+
+btns.forEach((btnItem, index) => {
+    btnItem.addEventListener('click', () => {
+        //смена стрелок
+        btns.forEach((btnItem) => {
+            btnItem.classList.remove('feature__link_active');
+        });
+        btnItem.classList.add('feature__link_active');
+
+        //смена показа блока со списком характеристик
+        lists.forEach((listItem) => {
+            listItem.classList.add('hidden');
+        });
+        lists[index].classList.toggle('hidden');
+    });
+});
